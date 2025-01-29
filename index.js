f0a441ba5bdd35722cb036328c4222cbee970842
shiva kumar <44138636+shiva0217@users.noreply.github.com>
2025-01-29 10:51:07 +0530

    fourth commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { v4: uuidv4 } = require('uuid');
 const mongoose = require('mongoose');
 const cors = require('cors');
